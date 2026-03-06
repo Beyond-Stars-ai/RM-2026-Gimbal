51b86dcb9b848c18c27a2872b59a39c64a8957b6
Beyond-Stars-ai <3220852648@qq.com>
2026-03-06 15:07:26 +0800

    检验分支

--- Src/main.c
+++ Src/main.c
@@ -158,7 +158,7 @@ int main(void)
   MX_USART1_UART_Init();
   MX_USART3_UART_Init();
   MX_TIM6_Init();
-//  MX_IWDG_Init();
+  //  MX_IWDG_Init();
   MX_I2C3_Init();
   MX_SPI1_Init();
   MX_TIM10_Init();
