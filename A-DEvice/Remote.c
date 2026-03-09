a9b25926fea951d49906c8abf576455853587d4a
Beyond-Stars-ai <3220852648@qq.com>
2026-03-09 20:16:35 +0800

    处理遥控系统3.9

--- A-DEvice/Remote.c
+++ A-DEvice/Remote.c
@@ -4,7 +4,7 @@
 //===============变量区
 extern UART_HandleTypeDef huart3;
 extern DMA_HandleTypeDef hdma_usart3_rx;
-const RC_ctrl_t *local_rc_ctrl;	
+const RC_ctrl_t *local_rc_ctrl;
 static uint8_t sbus_rx_buf[2][SBUS_RX_BUF_NUM];
 RC_ctrl_t rc_ctrl;
 
@@ -13,7 +13,6 @@ const RC_ctrl_t *remote_GetControlPoint(void)
 {
     return &rc_ctrl;
 }
-
 
 static void remote_SbusToRC(volatile const uint8_t *sbus_buf, RC_ctrl_t *rc_ctrl)
 {
@@ -25,17 +24,18 @@ static void remote_SbusToRC(volatile const uint8_t *sbus_buf, RC_ctrl_t *rc_ctrl
     rc_ctrl->rc.ch[0] = (sbus_buf[0] | (sbus_buf[1] << 8)) & 0x07ff;        //!< Channel 0
     rc_ctrl->rc.ch[1] = ((sbus_buf[1] >> 3) | (sbus_buf[2] << 5)) & 0x07ff; //!< Channel 1
     rc_ctrl->rc.ch[2] = ((sbus_buf[2] >> 6) | (sbus_buf[3] << 2) |          //!< Channel 2
-                         (sbus_buf[4] << 10)) &0x07ff;
+                         (sbus_buf[4] << 10)) &
+                        0x07ff;
     rc_ctrl->rc.ch[3] = ((sbus_buf[4] >> 1) | (sbus_buf[5] << 7)) & 0x07ff; //!< Channel 3
-    rc_ctrl->rc.s[0] = ((sbus_buf[5] >> 4) & 0x0003);                  //!< Switch left
-    rc_ctrl->rc.s[1] = ((sbus_buf[5] >> 4) & 0x000C) >> 2;                       //!< Switch right
+    rc_ctrl->rc.s[0] = ((sbus_buf[5] >> 4) & 0x0003);                       //!< Switch left
+    rc_ctrl->rc.s[1] = ((sbus_buf[5] >> 4) & 0x000C) >> 2;                  //!< Switch right
     rc_ctrl->mouse.x = sbus_buf[6] | (sbus_buf[7] << 8);                    //!< Mouse X axis
     rc_ctrl->mouse.y = sbus_buf[8] | (sbus_buf[9] << 8);                    //!< Mouse Y axis
     rc_ctrl->mouse.z = sbus_buf[10] | (sbus_buf[11] << 8);                  //!< Mouse Z axis
     rc_ctrl->mouse.press_l = sbus_buf[12];                                  //!< Mouse Left Is Press ?
     rc_ctrl->mouse.press_r = sbus_buf[13];                                  //!< Mouse Right Is Press ?
     rc_ctrl->key.v = sbus_buf[14] | (sbus_buf[15] << 8);                    //!< KeyBoard value
-    rc_ctrl->rc.ch[4] = sbus_buf[16] | (sbus_buf[17] << 8);                 //NULL
+    rc_ctrl->rc.ch[4] = sbus_buf[16] | (sbus_buf[17] << 8);                 // NULL
 
     rc_ctrl->rc.ch[0] -= RC_CH_VALUE_OFFSET;
     rc_ctrl->rc.ch[1] -= RC_CH_VALUE_OFFSET;
@@ -47,17 +47,17 @@ static void remote_SbusToRC(volatile const uint8_t *sbus_buf, RC_ctrl_t *rc_ctrl
 //===============公共函数
 void Remote_Init(void)
 {
-	local_rc_ctrl = remote_GetControlPoint();
-	Dbus_Dma_Init(sbus_rx_buf[0], sbus_rx_buf[1], SBUS_RX_BUF_NUM);
+    local_rc_ctrl = remote_GetControlPoint();
+    Dbus_Dma_Init(sbus_rx_buf[0], sbus_rx_buf[1], SBUS_RX_BUF_NUM);
 }
 
 void Remote_UART_IDLE_Callback(void)
 {
-    if(huart3.Instance->SR & UART_FLAG_RXNE)
+    if (huart3.Instance->SR & UART_FLAG_RXNE)
     {
         __HAL_UART_CLEAR_PEFLAG(&huart3);
     }
-    else if(USART3->SR & UART_FLAG_IDLE)
+    else if (USART3->SR & UART_FLAG_IDLE)
     {
         static uint16_t this_time_rx_len = 0;
 
@@ -66,23 +66,23 @@ void Remote_UART_IDLE_Callback(void)
         if ((hdma_usart3_rx.Instance->CR & DMA_SxCR_CT) == RESET)
         {
             /* Current memory buffer used is Memory 0 */
-    
-            //disable DMA
+
+            // disable DMA
             __HAL_DMA_DISABLE(&hdma_usart3_rx);
 
-            //get receive data length, length = set_data_length - remain_length
+            // get receive data length, length = set_data_length - remain_length
             this_time_rx_len = SBUS_RX_BUF_NUM - hdma_usart3_rx.Instance->NDTR;
 
-            //reset set_data_lenght
+            // reset set_data_lenght
             hdma_usart3_rx.Instance->NDTR = SBUS_RX_BUF_NUM;
 
-            //set memory buffer 1
+            // set memory buffer 1
             hdma_usart3_rx.Instance->CR |= DMA_SxCR_CT;
-            
-            //enable DMA
+
+            // enable DMA
             __HAL_DMA_ENABLE(&hdma_usart3_rx);
 
-            if(this_time_rx_len == RC_FRAME_LENGTH)
+            if (this_time_rx_len == RC_FRAME_LENGTH)
             {
                 remote_SbusToRC(sbus_rx_buf[0], &rc_ctrl);
             }
@@ -90,50 +90,67 @@ void Remote_UART_IDLE_Callback(void)
         else
         {
             /* Current memory buffer used is Memory 1 */
-            //disable DMA
+            // disable DMA
             __HAL_DMA_DISABLE(&hdma_usart3_rx);
 
-            //get receive data length, length = set_data_length - remain_length
+            // get receive data length, length = set_data_length - remain_length
             this_time_rx_len = SBUS_RX_BUF_NUM - hdma_usart3_rx.Instance->NDTR;
 
-            //reset set_data_lenght
+            // reset set_data_lenght
             hdma_usart3_rx.Instance->NDTR = SBUS_RX_BUF_NUM;
 
-            //set memory buffer 0
+            // set memory buffer 0
             DMA1_Stream1->CR &= ~(DMA_SxCR_CT);
-            
-            //enable DMA
+
+            // enable DMA
             __HAL_DMA_ENABLE(&hdma_usart3_rx);
 
-            if(this_time_rx_len == RC_FRAME_LENGTH)
+            if (this_time_rx_len == RC_FRAME_LENGTH)
             {
                 remote_SbusToRC(sbus_rx_buf[1], &rc_ctrl);
             }
         }
     }
 }
-void CToC_MasterSendData(	int16_t data1, int16_t data2, 
-															int16_t data3, int16_t data4, 
-															CAN_HandleTypeDef *hcan)
+void CToC_MasterSendData(int16_t data1, int16_t data2,
+                         int16_t data3, int16_t data4,
+                         int16_t data5, int16_t data6,
+                         CAN_HandleTypeDef *hcan)
 {
-	CAN_TxHeaderTypeDef  Tx_Message;
-	uint8_t	Can_Send_Data[8];
-  uint32_t send_mail_box;
-	
-	Tx_Message.StdId = 0x149;
-	Tx_Message.RTR = CAN_RTR_DATA;//数据帧
-	Tx_Message.IDE = CAN_ID_STD;//标准格式
-	Tx_Message.DLC = 0x08;//8字节数据段
-  Can_Send_Data[0] = data1 >> 8;
-  Can_Send_Data[1] = data1;
-  Can_Send_Data[2] = data2 >> 8;
-  Can_Send_Data[3] = data2;
-  Can_Send_Data[4] = data3 >> 8;
-  Can_Send_Data[5] = data3;
-  Can_Send_Data[6] = data4 >> 8;
-  Can_Send_Data[7] = data4;
-	
-  HAL_CAN_AddTxMessage(hcan, &Tx_Message, Can_Send_Data, &send_mail_box);
+    CAN_TxHeaderTypeDef Tx_Message;
+    uint8_t Can_Send_Data[8];
+    uint32_t send_mail_box;
+
+    Tx_Message.StdId = 0x149;
+    Tx_Message.RTR = CAN_RTR_DATA; // 数据帧
+    Tx_Message.IDE = CAN_ID_STD;   // 标准格式
+    Tx_Message.DLC = 0x08;         // 8字节数据段
+    Can_Send_Data[0] = data1 >> 8;
+    Can_Send_Data[1] = data1;
+    Can_Send_Data[2] = data2 >> 8;
+    Can_Send_Data[3] = data2;
+    Can_Send_Data[4] = data3 >> 8;
+    Can_Send_Data[5] = data3;
+    Can_Send_Data[6] = data4 >> 8;
+    Can_Send_Data[7] = data4;
+    HAL_CAN_AddTxMessage(hcan, &Tx_Message, Can_Send_Data, &send_mail_box);
+
+    Tx_Message.StdId = 0x189;  // 使用不同的ID
+    Can_Send_Data[0] = data5 >> 8;
+    Can_Send_Data[1] = data5;
+    Can_Send_Data[2] = data6 >> 8;
+    Can_Send_Data[3] = data6;
+    Can_Send_Data[4] = 0;
+    Can_Send_Data[5] = 0;
+    Can_Send_Data[6] = 0;
+    Can_Send_Data[7] = 0;
+    HAL_CAN_AddTxMessage(hcan, &Tx_Message, Can_Send_Data, &send_mail_box);
+
 }
 
 
+// else if(ID==0x189)//接收遥控器控制数据
+// 	{
+// 		Remote_RxData.Remote_RS=(int16_t)((uint16_t)Data[2]<<8 | Data[3]);
+// 		Remote_RxData.Remote_LS=(int16_t)((uint16_t)Data[0]<<8 | Data[1]);
+// 	}
--- A-DEvice/Remote.h
+++ A-DEvice/Remote.h
@@ -1,6 +1,6 @@
 #ifndef REMOTE_CONTROL_H
 #define REMOTE_CONTROL_H
-#include <stdint.h> 
+#include <stdint.h>
 #include "bsp_usart.h"
 #include "can.h"
 
@@ -8,34 +8,34 @@
 
 #define RC_FRAME_LENGTH 18u
 
-#define RC_CH_VALUE_MIN         ((uint16_t)364)
-#define RC_CH_VALUE_OFFSET      ((uint16_t)1024)
-#define RC_CH_VALUE_MAX         ((uint16_t)1684)
+#define RC_CH_VALUE_MIN ((uint16_t)364)
+#define RC_CH_VALUE_OFFSET ((uint16_t)1024)
+#define RC_CH_VALUE_MAX ((uint16_t)1684)
 
 /* ----------------------- RC Switch Definition----------------------------- */
-#define RC_SW_UP                ((uint16_t)1)
-#define RC_SW_MID               ((uint16_t)3)
-#define RC_SW_DOWN              ((uint16_t)2)
-#define switch_is_down(s)       (s == RC_SW_DOWN)
-#define switch_is_mid(s)        (s == RC_SW_MID)
-#define switch_is_up(s)         (s == RC_SW_UP)
+#define RC_SW_UP ((uint16_t)1)
+#define RC_SW_MID ((uint16_t)3)
+#define RC_SW_DOWN ((uint16_t)2)
+#define switch_is_down(s) (s == RC_SW_DOWN)
+#define switch_is_mid(s) (s == RC_SW_MID)
+#define switch_is_up(s) (s == RC_SW_UP)
 /* ----------------------- PC Key Definition-------------------------------- */
-#define KEY_PRESSED_OFFSET_W            ((uint16_t)1 << 0)
-#define KEY_PRESSED_OFFSET_S            ((uint16_t)1 << 1)
-#define KEY_PRESSED_OFFSET_A            ((uint16_t)1 << 2)
-#define KEY_PRESSED_OFFSET_D            ((uint16_t)1 << 3)
-#define KEY_PRESSED_OFFSET_SHIFT        ((uint16_t)1 << 4)
-#define KEY_PRESSED_OFFSET_CTRL         ((uint16_t)1 << 5)
-#define KEY_PRESSED_OFFSET_Q            ((uint16_t)1 << 6)
-#define KEY_PRESSED_OFFSET_E            ((uint16_t)1 << 7)
-#define KEY_PRESSED_OFFSET_R            ((uint16_t)1 << 8)
-#define KEY_PRESSED_OFFSET_F            ((uint16_t)1 << 9)
-#define KEY_PRESSED_OFFSET_G            ((uint16_t)1 << 10)
-#define KEY_PRESSED_OFFSET_Z            ((uint16_t)1 << 11)
-#define KEY_PRESSED_OFFSET_X            ((uint16_t)1 << 12)
-#define KEY_PRESSED_OFFSET_C            ((uint16_t)1 << 13)
-#define KEY_PRESSED_OFFSET_V            ((uint16_t)1 << 14)
-#define KEY_PRESSED_OFFSET_B            ((uint16_t)1 << 15)
+#define KEY_PRESSED_OFFSET_W ((uint16_t)1 << 0)
+#define KEY_PRESSED_OFFSET_S ((uint16_t)1 << 1)
+#define KEY_PRESSED_OFFSET_A ((uint16_t)1 << 2)
+#define KEY_PRESSED_OFFSET_D ((uint16_t)1 << 3)
+#define KEY_PRESSED_OFFSET_SHIFT ((uint16_t)1 << 4)
+#define KEY_PRESSED_OFFSET_CTRL ((uint16_t)1 << 5)
+#define KEY_PRESSED_OFFSET_Q ((uint16_t)1 << 6)
+#define KEY_PRESSED_OFFSET_E ((uint16_t)1 << 7)
+#define KEY_PRESSED_OFFSET_R ((uint16_t)1 << 8)
+#define KEY_PRESSED_OFFSET_F ((uint16_t)1 << 9)
+#define KEY_PRESSED_OFFSET_G ((uint16_t)1 << 10)
+#define KEY_PRESSED_OFFSET_Z ((uint16_t)1 << 11)
+#define KEY_PRESSED_OFFSET_X ((uint16_t)1 << 12)
+#define KEY_PRESSED_OFFSET_C ((uint16_t)1 << 13)
+#define KEY_PRESSED_OFFSET_V ((uint16_t)1 << 14)
+#define KEY_PRESSED_OFFSET_B ((uint16_t)1 << 15)
 /* ----------------------- Data Struct ------------------------------------- */
 typedef __packed struct
 {
@@ -62,14 +62,14 @@ typedef __packed struct
 /* ----------------------- Internal Data ----------------------------------- */
 
 extern const RC_ctrl_t *remote_GetControlPoint(void);
-void Remote_Init(void);//遥控器初始化
+void Remote_Init(void); // 遥控器初始化
 void Remote_UART_IDLE_Callback(void);
 #include <stdint.h>
 #include "can.h"
 
-
-void CToC_MasterSendData(	int16_t data1, int16_t data2, 
-													int16_t data3, int16_t data4, 
-													CAN_HandleTypeDef *hcan);
+void CToC_MasterSendData(int16_t data1, int16_t data2,
+                         int16_t data3, int16_t data4,
+                         int16_t data5, int16_t data6,
+                         CAN_HandleTypeDef *hcan);
 
 #endif
