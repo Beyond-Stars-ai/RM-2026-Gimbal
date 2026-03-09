ae947ac5a6b922fe1ad7437d8628e2dc2dd5fbf9
Beyond-Stars-ai <3220852648@qq.com>
2026-03-09 19:36:18 +0800

    加载裁判系统

---
+++ A-APPlication/Referee System.c
@@ -0,0 +1,17 @@
+#include "Referee System.h"
+
+// void Referee_System_Init(void)
+// {
+// 	// // 初始化舵机控制系统
+// 	// Gimbal_PoseCalc_Init();
+// 	// Gimbal_YawBig_Init(0);
+// 	// Gimbal_YawSmall_Init(0);
+// }
+
+// void Referee_System_Update(void)
+// {
+// 	// // 更新舵机控制系统
+// 	// Gimbal_PoseCalc_Update();
+// 	// Gimbal_YawBig_Control();
+// 	// Gimbal_YawSmall_Control();
+// }
---
+++ A-APPlication/Referee System.h
@@ -0,0 +1,21 @@
+#ifndef REFEREE_SYSTEM_H
+#define REFEREE_SYSTEM_H
+
+#include "main.h"
+#include "Gimbal_PoseCalc.h"
+#include "Gimbal_Yaw_Big.h"
+#include "Gimbal_Yaw_Small.h"
+
+// typedef struct
+// {
+// 	uint8_t SOF;           //固定为0xA5
+// 	uint16_t data_length;  //data字节数
+// 	uint8_t seq;		   //包序号
+// 	uint8_t CRC8;
+// }fram_head;
+
+// void Referee_System_Init(void);
+// void Referee_System_Update(void);
+
+#endif // REFEREE_SYSTEM_H
+
