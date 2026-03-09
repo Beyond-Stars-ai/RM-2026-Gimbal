7baebab39d5ab59e0d516ee17ffaa84f4f88b03f
Beyond-Stars-ai <3220852648@qq.com>
2026-03-09 21:37:49 +0800

    增加串口UART1（芯片上的uart6）

--- Src/main.c
+++ Src/main.c
@@ -158,12 +158,13 @@ int main(void)
   MX_USART1_UART_Init();
   MX_USART3_UART_Init();
   MX_TIM6_Init();
-  //  MX_IWDG_Init();
+  // MX_IWDG_Init();
   MX_I2C3_Init();
   MX_SPI1_Init();
   MX_TIM10_Init();
   MX_TIM5_Init();
   MX_TIM4_Init();
+  MX_USART6_UART_Init();
   /* USER CODE BEGIN 2 */
 	HAL_TIM_Base_Start_IT(&htim6);
   Can_Filter_Init();
